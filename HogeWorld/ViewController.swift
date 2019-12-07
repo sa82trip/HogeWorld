7ff5633a8e3b965b8fc5f3f944b727e755587aa6
sa82trip <sa82trip@gmail.com>
2019-12-07 16:38:09 +0900

    Initial Commit

---
+++ HogeWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HogeWorld
+//
+//  Created by GJS on 2019/12/07.
+//  Copyright © 2019 GJS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
