7494ce5163e00e71a5905299ded876e4b4740ad0
sa82trip <sa82trip@gmail.com>
2019-12-07 21:05:50 +0900

    this is first commit

--- HogeWorld/AppDelegate.swift
+++ HogeWorld/AppDelegate.swift
@@ -10,7 +10,11 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    //이거 넣어줘야 ios11로 바꿨을 때 된다
+    //그리고 scene delegate지워야함
+    //info.plist가서 scene manifest지워라
+    
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -20,17 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+    
 
 
 }
--- HogeWorld/ViewController.swift
+++ HogeWorld/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var informationLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField2: UITextField!
+    
+    var members:[Member] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        print("Gil select")
+        var i = 0
+        for member in members{
+            print(i,member.name)
+            print(i,member.age)
+            i+=1
+        }
+        
+    }
+    
+    @IBAction func createButtonTapped(_ sender: UIButton) {
+        guard let name = nameTextField.text else { return }
+        guard let age = Int(ageTextField2.text ?? "0") else { return }
+        
+        print("Gil create")
+        informationLabel.text = (informationLabel.text ?? "")+"create button tapped\r"
+        let member: Member = Member(name: name, age: age)
+        print(member.name)
+        print(member.age)
+        members.append(member)
+        informationLabel.text = member.name + " is added"
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: UIButton) {
+        print("Gil delete")
+        informationLabel.text = (informationLabel.text ?? "")+"delete button tapped\r"
+        
+        informationLabel.text = members[members.count-1].name + " is deleted"
+        members.remove(at: members.count-1)
+        
+
+    }
+    func hideKeyboard(){
+        nameTextField.resignFirstResponder()
+        ageTextField2.resignFirstResponder()
+    }
 
 }
+
+class Member {
+    var name: String
+    var age: Int?
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+    
+}
+
+
 
